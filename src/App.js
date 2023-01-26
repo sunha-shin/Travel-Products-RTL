c7f2d249e85ae5955e642f07269a2d6b25a1be62
Summer Shin <shinsunha11@gmail.com>
2023-01-26 16:27:23 -0500

    Modify UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
+import OrderPage from "./pages/OrderPage/OrderPage.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div style={{padding:"4rem"}}>
+            <OrderPage/>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/OrderPage/Type.js
+++ src/pages/OrderPage/Type.js
@@ -1,41 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import Products from "./Products.js";
 import Options from "./Options.js";
 import ErrorBanner from "../../components/ErrorBanner.js";
 
-function Type({ orderType }) {
-  const [items, setItems] = useState([]);
-  const [error, setError] = useState(false);
+function Type({orderType}) {
+    const [items, setItems] = useState([]);
+    const [error, setError] = useState(false);
 
-  useEffect(() => {
-    loadItems(orderType);
-  }, [orderType]);
+    useEffect(() => {
+        loadItems(orderType);
+    }, [orderType]);
 
-  const loadItems = async (orderType) => {
-    try {
-      let response = await axios.get(`http://localhost:5000/${orderType}`);
-      setItems(response.data)
-    } catch (error) {
-      setError(true);
+    const loadItems = async (orderType) => {
+        try {
+            let response = await axios.get(`http://localhost:5000/${orderType}`);
+            setItems(response.data)
+        } catch (error) {
+            setError(true);
+        }
+    };
+
+    if (error) {
+        return <ErrorBanner message='Error'/>
     }
-  };
 
-  if(error) {
-    return <ErrorBanner message='Error'/>
-  }
+    const ItemComponents = (orderType === "products" ? Products : Options);
 
-  const ItemComponents = (orderType === "products" ? Products : Options);
+    const optionsItems = items.map((item) => (
+        <ItemComponents
+            key={item.name}
+            name={item.name}
+            imagePath={item.imagePath}
+        />
+    ));
 
-  const optionsItems = items.map((item) => (
-    <ItemComponents
-      key={item.name}
-      name={item.name}
-      imagePath={item.imagePath}
-    />
-  ));
-
-  return <div>{optionsItems}</div>;
+    return (
+        <>
+            <h2>Travel Products</h2>
+            <p>Price</p>
+            <p>Total Price: </p>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: orderType === "options" && "columm "
+                }}
+            >
+                {optionsItems}
+            </div>
+        </>
+    );
 }
 
 export default Type;
